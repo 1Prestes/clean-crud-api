c19bed1553b3b185bf792886385d5302f15b11d4
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2021-06-09 16:06:23 -0300

    test: ensure SignUpController return 400 if no passwordConfirmation is provided

--- src/entities/controllers/user.spec.ts
+++ src/entities/controllers/user.spec.ts
@@ -53,4 +53,23 @@ describe('UserController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('password not provided'))
   })
+
+  test('Should return 400 if no passwordConfirmation is provided', () => {
+    const sut = makeSut()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.create(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(
+      new Error('passwordConfirmation not provided')
+    )
+  })
 })
