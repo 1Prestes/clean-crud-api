66e72142929b42fa6e8147af9d0226628679f5fe
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2021-06-10 15:01:48 -0300

    feat: ensure UserController returns 400 if an invalid email is provided

--- src/entities/controllers/user.ts
+++ src/entities/controllers/user.ts
@@ -1,13 +1,20 @@
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
+import { EmailValidator } from '../protocols/email-validator'
 import { HttpRequest } from '../protocols/http'
 import { UserControllerModel } from '../protocols/user-controller'
 
 export default class UserController implements UserControllerModel {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   create (httpRequest: HttpRequest): any {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    const { password, passwordConfirmation } = httpRequest.body
+    const { email, password, passwordConfirmation } = httpRequest.body
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
@@ -17,6 +24,10 @@ export default class UserController implements UserControllerModel {
 
     if (password !== passwordConfirmation) {
       return badRequest(new InvalidParamError('passwordConfirmation'))
+    }
+
+    if (!this.emailValidator.isValid(email)) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
